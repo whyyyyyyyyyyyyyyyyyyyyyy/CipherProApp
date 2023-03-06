0f5b123da03b980610555981c08315dcae331bf0
whyyyyyyyyyyyyyyyyyyyyyy <milnew@macstudent.com.au>
2023-03-06 15:32:04 +1100

    Initial Commit

---
+++ CipherProApp/CipherProAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CipherProAppApp.swift
+//  CipherProApp
+//
+//  Created by William Milne on 6/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct CipherProAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CipherProApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CipherProApp
+//
+//  Created by William Milne on 6/3/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
