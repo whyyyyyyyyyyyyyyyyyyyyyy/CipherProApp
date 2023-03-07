acdca3be8c7708126d484d6707d514474b0844e0
whyyyyyyyyyyyyyyyyyyyyyy <milnew@macstudent.com.au>
2023-03-07 11:31:14 +1100

    Added a way to update the list of keys

--- CipherProApp/cipherStorage.swift
+++ CipherProApp/cipherStorage.swift
@@ -7,14 +7,8 @@
 
 import Foundation
 
-
-let keyStore = [
-    Key(name: "Unnamed", a: "", b: "", c: "", d: "", e: "", f: "", g: "", h: "", i: "", j: "", k: "", l: "", m: "", n: "", o: "", p: "", q: "", r: "", s: "", t: "", u: "", v: "", w: "", x: "", y: "", z: ""),
-    Key(name: "Unnamed 2", a: "", b: "", c: "", d: "", e: "", f: "", g: "", h: "", i: "", j: "", k: "", l: "", m: "", n: "", o: "", p: "", q: "", r: "", s: "", t: "", u: "", v: "", w: "", x: "", y: "", z: "")
-]
-
-struct Key: Identifiable {
-    var id = UUID()
+public struct Key: Identifiable {
+    public var id = UUID()
     var name: String
     var a: String
     var b: String
@@ -43,6 +37,15 @@ struct Key: Identifiable {
     var y: String
     var z: String
 }
+
+
+public var keyStore = [
+    Key(name: "hey", a: "", b: "", c: "", d: "", e: "", f: "", g: "", h: "", i: "", j: "", k: "", l: "", m: "", n: "", o: "", p: "", q: "", r: "", s: "", t: "", u: "", v: "", w: "", x: "", y: "", z: ""),
+    Key(name: "Unnamed 2", a: "", b: "", c: "", d: "", e: "", f: "", g: "", h: "", i: "", j: "", k: "", l: "", m: "", n: "", o: "", p: "", q: "", r: "", s: "", t: "", u: "", v: "", w: "", x: "", y: "", z: ""),
+    Key(name: "hilykutfjy", a: "", b: "", c: "", d: "", e: "", f: "", g: "", h: "", i: "", j: "", k: "", l: "", m: "", n: "", o: "", p: "", q: "", r: "", s: "", t: "", u: "", v: "", w: "", x: "", y: "", z: ""),
+    Key(name: "wooorld", a: "", b: "", c: "", d: "", e: "", f: "", g: "", h: "", i: "", j: "", k: "", l: "", m: "", n: "", o: "", p: "", q: "", r: "", s: "", t: "", u: "", v: "", w: "", x: "", y: "", z: "")
+]
+
 
 //struct keyRow: View {
 //    var uyt = Key.self
--- CipherProApp/listScreen.swift
+++ CipherProApp/listScreen.swift
@@ -7,36 +7,8 @@
 
 import SwiftUI
 
-struct keys: Identifiable {
-    var id = UUID()
-    var name: String
-    var a: String
-    var b: String
-    var c: String
-    var d: String
-    var e: String
-    var f: String
-    var g: String
-    var h: String
-    var i: String
-    var j: String
-    var k: String
-    var l: String
-    var m: String
-    var n: String
-    var o: String
-    var p: String
-    var q: String
-    var r: String
-    var s: String
-    var t: String
-    var u: String
-    var v: String
-    var w: String
-    var x: String
-    var y: String
-    var z: String
-}
+
+
 
 struct keyRow: View {
     var key: Key
@@ -50,21 +22,35 @@ struct keyRow: View {
     }
 }
 
+func counting() -> Int{
+    return keyStore.count - 1
+}
+
+func addTo() {
+    keyStore.append(Key(name: "added", a: "", b: "", c: "", d: "", e: "", f: "", g: "", h: "", i: "", j: "", k: "", l: "", m: "", n: "", o: "", p: "", q: "", r: "", s: "", t: "", u: "", v: "", w: "", x: "", y: "", z: ""))
+}
 
 struct listScreen: View {
-    let keyStore = [
-        Key(name: "hey", a: "", b: "", c: "", d: "", e: "", f: "", g: "", h: "", i: "", j: "", k: "", l: "", m: "", n: "", o: "", p: "", q: "", r: "", s: "", t: "", u: "", v: "", w: "", x: "", y: "", z: ""),
-        Key(name: "Unnamed 2", a: "", b: "", c: "", d: "", e: "", f: "", g: "", h: "", i: "", j: "", k: "", l: "", m: "", n: "", o: "", p: "", q: "", r: "", s: "", t: "", u: "", v: "", w: "", x: "", y: "", z: "")
-        ]
-        
+    
+    let on = true
         var body: some View {
-            NavigationView {
-                List(keyStore) { item in
-                    NavigationLink(destination: ciphermakeScreen(key: item)) {
-                        keyRow(key: item)
+            VStack {
+                NavigationView {
+                    List(keyStore) { item in
+                        NavigationLink(destination: ciphermakeScreen(key: item)) {
+                            keyRow(key: item)
+                        }
                     }
+                    .navigationBarTitle("Encryption keys")
                 }
-                .navigationBarTitle("Encryption keys")
+                Button() {
+                    if on == true {
+                        addTo()
+                    }
+                } label: {
+                    Text(keyStore[counting()].name)
+                    
+                }
             }
     }
 }
@@ -72,7 +58,10 @@ struct listScreen: View {
 struct ciphermakeScreen: View {
     var key: Key
     var body: some View {
-        Text(key.name)
+        VStack {
+            Text(key.name)
+            Text(keyStore[counting()].name)
+        }
     }
 }
 struct listScreen_Previews: PreviewProvider {
